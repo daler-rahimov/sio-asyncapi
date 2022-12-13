c935e10d0c2306d21281bcef23d8ea24b0cc8d40
Daler Rahimov <daler.edu@gmail.com>
2022-12-12 23:33:00 -0500

    remove unused imports

--- examples/downloader.py
+++ examples/downloader.py
@@ -8,8 +8,6 @@ from typing import Optional
 from pathlib import Path
 import pathlib
 
-from flask_socketio import SocketIO
-
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
